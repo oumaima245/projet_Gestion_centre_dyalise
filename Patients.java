df1eec8a2b3ea007734941581d3fd35ffe935c30
oumaima245 <98613152+oumaima245@users.noreply.github.com>
2023-01-11 22:01:39 +0100

    Update Patients.java

--- Patients.java
+++ Patients.java
@@ -1,84 +1,94 @@
 package classdoctor;
 
-public class Doctor {
-String Nom;
-String Prenom;
-int CIN;
-int date;
-String adress;
-int nemero;
-String genre ;
-String deplome ;
+public class Patients {
+	
+	String Nom;
+	String Prenom;
+	int CIN;
+	int date;
+	String adress;
+	int nemero;
+	String genre ;
+	int Num_p_urgence ;
+	int prix;
+	public Patients(String nom, String prenom, int cIN, int date, String adress, int nemero, String genre,
+			int num_p_urgence,int prix) {
+		Nom = nom;
+		Prenom = prenom;
+		CIN = cIN;
+		this.date = date;
+		this.adress = adress;
+		this.nemero = nemero;
+		this.genre = genre;
+		Num_p_urgence = num_p_urgence;
+		this.prix=prix;
+	}
+	public Patients() {
+		Nom = "NULL";
+		Prenom = "NULL";
+		CIN = 0;
+		this.date = 0;
+		this.adress = "NULL";
+		this.nemero = 0;
+		this.genre = "NULL";
+		Num_p_urgence = 0;
+		this.prix=0;
 
-public Doctor(String nom, String prenom, int cIN, int date, String adress, int nemero,String genre ,String deplome) {
-	Nom = nom;
-	Prenom = prenom;
-	CIN = cIN;
-	this.date = date;
-	this.adress = adress;
-	this.nemero = nemero;
-	this.genre=genre ;
-	this.deplome=deplome ;
-}
-
-public Doctor() {
-	Nom = "NULL";
-	Prenom = "NULL";
-	CIN = 0;
-	this.date = 0;
-	this.adress = "NULL";
-	this.nemero = 0;
-	this.genre="NULL";
-	this.deplome="NULL" ;
-}
-public String getNom() {
-	return Nom;
-}
-public void setNom(String nom) {
-	Nom = nom;
-}
-public String getPrenom() {
-	return Prenom;
-}
-public void setPrenom(String prenom) {
-	Prenom = prenom;
-}
-public int getCIN() {
-	return CIN;
-}
-public void setCIN(int cIN) {
-	CIN = cIN;
-}
-public int getDate() {
-	return date;
-}
-public void setDate(int date) {
-	this.date = date;
-}
-public String getAdress() {
-	return adress;
-}
-public void setAdress(String adress) {
-	this.adress = adress;
-}
-
-public int getNemero() {
-	return nemero;
-}
-public void setNemero(int nemero) {
-	this.nemero = nemero;
-}
-public String getGenre() {
-	return genre;
-}
-public void setGenre(String genre) {
-	this.genre = genre;
-}
-public String getDeplome() {
-	return deplome;
-}
-public void setDeplome(String deplome) {
-	this.deplome = deplome;
-}
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public String getPrenom() {
+		return Prenom;
+	}
+	public void setPrenom(String prenom) {
+		Prenom = prenom;
+	}
+	public int getCIN() {
+		return CIN;
+	}
+	public void setCIN(int cIN) {
+		CIN = cIN;
+	}
+	public int getDate() {
+		return date;
+	}
+	public void setDate(int date) {
+		this.date = date;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getNemero() {
+		return nemero;
+	}
+	public void setNemero(int nemero) {
+		this.nemero = nemero;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public int getNum_p_urgence() {
+		return Num_p_urgence;
+	}
+	public void setNum_p_urgence(int num_p_urgence) {
+		Num_p_urgence = num_p_urgence;
+	}
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	
 
 }
